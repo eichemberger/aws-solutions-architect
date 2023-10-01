471954ccda171facc7f8fb5b74e088a448286202
German Eichemberger <germaneichemberger@gmail.com>
2023-10-01 09:45:08 -0300

    adds output to vpc stack

--- src/infra/stacks/VPCStack.ts
+++ src/infra/stacks/VPCStack.ts
@@ -1,22 +1,30 @@
-import {Stack, StackProps} from 'aws-cdk-lib';
+import {CfnOutput, Stack, StackProps} from 'aws-cdk-lib';
 import {Construct} from 'constructs';
 import {randomUUID} from 'crypto';
-import {IpAddresses, Vpc} from "aws-cdk-lib/aws-ec2";
+import {IpAddresses, IVpc, Vpc} from "aws-cdk-lib/aws-ec2";
 
 /*
     * This stack creates a vpc
- */
+*/
 export class VPCStack extends Stack {
+
+    public vpc: IVpc;
 
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
         const suffix = randomUUID();
 
-        new Vpc(this, `vpc-${suffix}`, {
+        this.vpc = new Vpc(this, `vpc-${suffix}`, {
             vpcName: `vpc-${suffix}`,
             ipAddresses: IpAddresses.cidr('10.0.0.0/16'),
             natGateways: 0,
         });
+
+        new CfnOutput(this, 'vpcId', {
+            value: this.vpc.vpcId,
+            description: 'VPC ID',
+            exportName: 'vpc:id'
+        })
     }
 }
