cd9366baa87a6155cf2dfdac9079f652afa262e4
German Eichemberger <germaneichemberger@gmail.com>
2023-10-01 09:51:09 -0300

    adds a prometheus stack

--- src/infra/Launcher.ts
+++ src/infra/Launcher.ts
@@ -1,19 +1,23 @@
 import { App } from 'aws-cdk-lib';
 import {VPCStack} from "./stacks/VPCStack";
 import {UIDeploymentStack} from "./stacks/UIDeploymentStack";
+import {PrometheusStack} from "./stacks/PrometheusStack";
 /*
 import {S3NotificationsStack} from "./stacks/S3NotificationsStack";
 import {DynamoDBStreamLambdaStack} from "./stacks/DynamoDBStreamLambdaStack";
 import {UIDeploymentStack} from "./stacks/UIDeploymentStack";
  */
 
-const app = new App(); 
+const app = new App();
 
 /*
+new UIDeploymentStack(app, 'UIDeploymentStack');
 new S3NotificationsStack(app, 'S3Stack');
 new DynamoDBStreamLambdaStack(app, 'DynamoDBStreamLambdaStack');
 new UIDeploymentStack(app, 'UIDeploymentStack');
 */
-new UIDeploymentStack(app, 'UIDeploymentStack');
 
-// new VPCStack(app, 'VPCStack');
+const vpcStack = new VPCStack(app, 'VPCStack');
+new PrometheusStack(app, 'PrometheusStack', {
+    vpc: vpcStack.vpc,
+})
---
+++ src/infra/stacks/PrometheusStack.ts
@@ -0,0 +1,53 @@
+import {CfnOutput, Stack, StackProps} from 'aws-cdk-lib';
+import {Construct} from "constructs";
+import {
+    AmazonLinuxGeneration,
+    AmazonLinuxImage,
+    Instance,
+    InstanceClass,
+    InstanceSize,
+    InstanceType,
+    IVpc,
+    Port,
+    SubnetType
+} from "aws-cdk-lib/aws-ec2";
+import * as path from "path";
+import * as fs from "fs";
+
+/*
+    * This is a stack for Prometheus
+    * It will create an EC2 instance with Prometheus installed
+ */
+interface PrometheusStackProps extends StackProps {
+    vpc: IVpc,
+}
+
+export class PrometheusStack extends Stack {
+    constructor(scope: Construct, id: string, props: PrometheusStackProps) {
+        super(scope, id, props);
+
+        const prometheusScriptPath = path.join(__dirname, '..', 'scripts', 'prometheus-install.sh');
+        const prometheusScriptData = fs.readFileSync(prometheusScriptPath, 'utf8');
+
+        const ec2Instance = new Instance(this, 'prometheus-instance', {
+            vpc: props.vpc,
+            instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
+            vpcSubnets: {
+                subnetType: SubnetType.PUBLIC,
+            },
+            machineImage: new AmazonLinuxImage({
+                generation: AmazonLinuxGeneration.AMAZON_LINUX_2
+            }),
+        });
+
+        ec2Instance.connections.allowFromAnyIpv4(Port.tcp(9090), 'Allow Prometheus Access');
+
+        ec2Instance.addUserData(prometheusScriptData);
+
+        new CfnOutput(this, 'prometheusIp', {
+            value: ec2Instance.instancePublicIp,
+            description: 'Prometheus IP',
+            exportName: 'prometheus:ip'
+        });
+    }
+}
