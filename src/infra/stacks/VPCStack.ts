17d5b53e3725d3048bd468839f6e98643bfafca6
German Eichemberger <germaneichemberger@gmail.com>
2023-10-01 19:58:17 -0300

    uses dynamic id

--- src/infra/stacks/VPCStack.ts
+++ src/infra/stacks/VPCStack.ts
@@ -2,6 +2,9 @@ import {CfnOutput, Stack, StackProps} from 'aws-cdk-lib';
 import {Construct} from 'constructs';
 import {randomUUID} from 'crypto';
 import {IpAddresses, IVpc, Vpc} from "aws-cdk-lib/aws-ec2";
+import {generateID} from "../../utils/IDGenerator";
+import {config} from "../../utils/Config";
+import {getExportName} from "../../utils/Utils";
 
 /*
     * This stack creates a vpc
@@ -15,16 +18,16 @@ export class VPCStack extends Stack {
 
         const suffix = randomUUID();
 
-        this.vpc = new Vpc(this, `vpc-${suffix}`, {
+        this.vpc = new Vpc(this, generateID('vpc'), {
             vpcName: `vpc-${suffix}`,
             ipAddresses: IpAddresses.cidr('10.0.0.0/16'),
             natGateways: 0,
         });
 
-        new CfnOutput(this, 'vpcId', {
+        new CfnOutput(this, generateID('vpc-id'), {
             value: this.vpc.vpcId,
             description: 'VPC ID',
-            exportName: 'vpc:id'
+            exportName: getExportName('vpcId'),
         })
     }
 }
